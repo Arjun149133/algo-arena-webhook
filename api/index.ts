9f98b080d776222c7bfc8c28d0116512afee7ec8
Arjun Yadav <arjun149133@gmail.com>
2025-04-14 21:01:50 +0530

    run logic change

--- api/index.ts
+++ api/index.ts
@@ -10,7 +10,6 @@ app.use(express.json());
 
 const prisma = new PrismaClient();
 
-const runMap: Map<string, string[]> = new Map();
 const tokenMap: Map<string, string | null> = new Map();
 const submissionMap: Map<
   string,
@@ -22,87 +21,28 @@ const submissionMap: Map<
   | "COMPILATION_ERROR"
 > = new Map();
 
-type RunData = {
-  token: string;
-};
-
 app.get("/", (req, res) => {
   res.send("Hello World!!");
-});
-
-app.post("/webhook/run/create", async (req, res) => {
-  const { submissionTokenArray: data } = req.body;
-
-  const newRunId = randomUUID();
-
-  runMap.set(newRunId, []);
-
-  data.map((value: RunData) => {
-    tokenMap.set(value.token, null);
-    runMap.get(newRunId)?.push(value.token);
-  });
-
-  res.status(200).json({ runId: newRunId });
 });
 
 app.post("/webhook/run/check", async (req, res) => {
   try {
     const data = req.body;
 
-    const runId = data.runId;
-
-    const tokenArray = runMap.get(runId);
-
-    if (!tokenArray) {
-      res.status(404).json({ error: "Run not found" });
-      return;
-    }
+    const submissionTokenArray: string[] = data.submissionTokenArray;
 
     let allCompleted = true;
-    for (const token of tokenArray) {
-      if (tokenMap.get(token) === null) {
+    for (const token of submissionTokenArray) {
+      const status = tokenMap.get(token);
+      if (status === null) {
         allCompleted = false;
         break;
       }
     }
 
     if (allCompleted) {
-      // const results: TestCaseResult[] = tokenArray.map(async (token) => {
-      //   const res = await axios.get(
-      //     `${process.env.JUDGE0_URL}/submissions/${token}?base64_encoded=false`
-      //   );
-
-      //   const data = res.data;
-
-      //   if (data.error) {
-      //     return {
-      //       result: data.error as string,
-      //       status: "PENDING",
-      //     };
-      //   }
-
-      //   if (data.status.description === "Accepted") {
-      //     return {
-      //       result: data.stdout as string,
-      //       status: "ACCEPTED",
-      //     };
-      //   } else if (data.status.description === "Wrong Answer") {
-      //     return {
-      //       result: data.stdout as string,
-      //       status: "REJECTED",
-      //     };
-      //   } else {
-      //     return {
-      //       result: data.stderr as string,
-      //       status: "REJECTED",
-      //       message: data.message as string,
-      //     };
-      //   }
-      // });
-
       res.status(200).json({
         status: "COMPLETED",
-        results: Array.from(tokenArray),
       });
     } else {
       res.status(200).json({ status: "PENDING" });
